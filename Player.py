82a73804f927fa435626333c890a5f2f63683652
noxiss <noxiss2015@gmail.com>
2015-04-07 09:00:17 -0400

    player all set

--- Player.py
+++ Player.py
@@ -3,35 +3,19 @@ import pygame, sys, math
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, size = [100,100]):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.upImages = [pygame.image.load("images/player/pu1.PNG"),
-                         pygame.image.load("images/player/pu2.PNG"),
-                         pygame.image.load("images/player/pu3.PNG"),
-                         pygame.image.load("images/player/pu2.PNG"),
-                         pygame.image.load("images/player/pu1.PNG")]
-        self.downImages = [pygame.image.load("images/player/pd1.PNG"),
-                           pygame.image.load("images/player/pd2.PNG"),
-                           pygame.image.load("images/player/pd3.PNG"),
-                           pygame.image.load("images/player/pd2.PNG"),
-                           pygame.image.load("images/player/pd1.PNG")]
-        self.leftImages = [pygame.image.load("images/player/pl1.PNG"),
-                           pygame.image.load("images/player/pl2.PNG"),
-                           pygame.image.load("images/player/pl3.PNG"),
-                           pygame.image.load("images/player/pl2.PNG"),
-                           pygame.image.load("images/player/pl1.PNG")]
-        self.rightImages = [pygame.image.load("images/player/pr1.PNG"),
+        self.images = [pygame.image.load("images/player/pr1.PNG"),
                             pygame.image.load("images/player/pr2.PNG"),
                             pygame.image.load("images/player/pr3.PNG"),
                             pygame.image.load("images/player/pr2.PNG"),
                             pygame.image.load("images/player/pr1.PNG")]
-        self.facing = "up"
         self.changed = False
-        self.images = self.upImages
-        self.baseImage = pygame.image.load("images/player/pu1.png")
-        self.baseImage = pygame.transform.scale(self.baseImage, size)
+        #self.baseImage = pygame.image.load("images/player/pu1.png")
+        #self.baseImage = pygame.transform.scale(self.baseImage, size)
         self.frame = 0
         self.maxFrame = len(self.images) - 1
         self.waitCount = 0
         self.maxWait = 60*.25
+        self.baseImage = self.images[self.frame]
         self.image = self.images[self.frame]
         self.rect = self.image.get_rect(center = pos)
         self.maxSpeed = 10
@@ -46,16 +30,6 @@ class Player(pygame.sprite.Sprite):
         self.animate()
         self.changed = False
         
-        mousePos = pygame.mouse.get_pos()
-        mousePosPlayerX = mousePos[0] - self.rect.center[0]
-        mousePosPlayerY = mousePos[1] - self.rect.center[1]
-        self.angle = ((math.atan2(mousePosPlayerY, mousePosPlayerX))/math.pi)*180
-        self.angle = -self.angle
-        rot_image = pygame.transform.rotate(self.baseImage, self.angle)
-        rot_rect = self.rect.copy()
-        rot_rect.center = rot_image.get_rect().center
-        rot_image = rot_image.subsurface(rot_rect)
-        self.image = rot_image
         
     def move(self):
         self.speed = [self.speedx, self.speedy]
@@ -73,41 +47,35 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.frame = 0
         
-        if self.changed:    
-            if self.facing == "up":
-                self.images = self.upImages
-            elif self.facing == "down":
-                self.images = self.downImages
-            elif self.facing == "right":
-                self.images = self.rightImages
-            elif self.facing == "left":
-                self.images = self.leftImages
-            
-            self.image = self.images[self.frame]
+        
+        self.baseImage = self.images[self.frame]    
+        mousePos = pygame.mouse.get_pos()
+        mousePosPlayerX = mousePos[0] - self.rect.center[0]
+        mousePosPlayerY = mousePos[1] - self.rect.center[1]
+        self.angle = ((math.atan2(mousePosPlayerY, mousePosPlayerX))/math.pi)*180
+        self.angle = -self.angle
+        rot_image = pygame.transform.rotate(self.baseImage, self.angle)
+        rot_rect = self.rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        rot_image = rot_image.subsurface(rot_rect)
+        self.image = rot_image
+        
     
     def go(self, direction):
         if direction == "up":
-            self.facing = "up"
-            self.changed = True
             self.speedy = -self.maxSpeed
         elif direction == "stop up":
             self.speedy = 0
         elif direction == "down":
-            self.facing = "down"
-            self.changed = True
             self.speedy = self.maxSpeed
         elif direction == "stop down":
             self.speedy = 0
             
         if direction == "right":
-            self.facing = "right"
-            self.changed = True
             self.speedx = self.maxSpeed
         elif direction == "stop right":
             self.speedx = 0
         elif direction == "left":
-            self.facing = "left"
-            self.changed = True
             self.speedx = -self.maxSpeed
         elif direction == "stop left":
             self.speedx = 0
