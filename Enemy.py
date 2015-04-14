e47f1497850e0677bc64159ababd00c0af79e3e0
noxiss <noxiss2015@gmail.com>
2015-04-14 13:40:55 -0400

    i

--- Enemy.py
+++ Enemy.py
@@ -2,7 +2,7 @@ import pygame, math
 from Default import Default
 
 class Enemy(pygame.sprite.Sprite):
-	def __init__(self, image, speed = [0,0], pos = [0,0]):
+    def __init__(self, image, speed = [0,0], pos = [0,0]):
             pygame.sprite.Sprite.__init__(self, self.containers)
             self.image = pygame.image.load(image)
             self.rect = self.image.get_rect()
@@ -12,25 +12,25 @@ class Enemy(pygame.sprite.Sprite):
             self.place(pos)
             self.radius = (int(self.rect.height/2.0 + self.rect.width/2.0)/2) - 1
             self.living = True
-		
-	def place(self, pos):
-		self.rect.center = pos
-		
-	def update(*args):
-		self = args[0]
-		width = args[1]
-		height = args[2]
+        
+    def place(self, pos):
+        self.rect.center = pos
+        
+    def update(*args):
+        self = args[0]
+        width = args[1]
+        height = args[2]
         player.center = args[3]
-		self.speed = [self.speedx, self.speedy]
-		self.move()
-		
-	def move(self):
-		self.rect = self.rect.move(self.speed)
+        self.speed = [self.speedx, self.speedy]
+        self.move()
+        
+    def move(self):
+        self.rect = self.rect.move(self.speed)
     
-	
-	def distance(self, pt):
-		x1 = self.rect.center[0]
-		y1 = self.rect.center[1]
-		x2 = pt[0]
-		y2 = pt[1]
-		return math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+    
+    def distance(self, pt):
+        x1 = self.rect.center[0]
+        y1 = self.rect.center[1]
+        x2 = pt[0]
+        y2 = pt[1]
+        return math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
